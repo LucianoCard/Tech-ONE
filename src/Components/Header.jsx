421afe381e8b7c03b75ca1c5663a512b61a1c60a
NadiaBadaraco <nadiab191200@gmail.com>
2025-10-25 02:55:34 -0300

    Mejora de estilo y funcionalidad del Header
    Implementé NavLink en lugar de Link para gestionar los enlaces.
    Con estilos logré hacer que el efecto del brillo del enlace solo esté en la página que está activa.
    Cambié la tipografía y quité el brillo del borde inferior para no saturar la estética

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,35 +2,37 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <Navbar
       collapseOnSelect
       expand="lg"
-      className="bg-body-tertiary glow-border border-top-0 p-4 "
+      className="bg-body-tertiary border-top-0 p-4 "
     >
       <Container className="d-flex justify-content-center">
-        <Navbar.Brand href="/home" className="tech-one">
+        <Navbar.Brand href="/home" className="navbar-fuente tech-one">
           Tech-One
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/Productos" className="tech-one">
+            <Nav.Link as={NavLink} to="/Productos" className="navbar-fuente tech-one">
               [Productos]
             </Nav.Link>
-            <Nav.Link href="/Ofertas" className="tech-one">
+            <Nav.Link as={NavLink} to="/Ofertas" className="navbar-fuente tech-one">
               [Ofertas]
             </Nav.Link>
-            <Nav.Link href="/Sobre-nosotros" className="tech-one">
+            <Nav.Link as={NavLink} to="/Sobre-nosotros" className="navbar-fuente tech-one">
               [Sobre nosotros]
             </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets" className="tech-one">
-              <i class="bi bi-person"></i>[Registrarse]
+            <Nav.Link as={NavLink} to="/registro" className="navbar-fuente tech-one">
+              <i className="bi bi-person"></i>[Registrarse]
             </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes" className="tech-one">
+            <Nav.Link eventKey={2} href="#memes" className="navbar-fuente tech-one">
               <i className="bi bi-cart"></i>[Carrito]
             </Nav.Link>
           </Nav>
